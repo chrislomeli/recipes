8a9fcab869d8c34df50bf84fa197ac5824eee710
Chris Lomeli <chrisjlomeli@gmail.com>
2017-06-06 11:25:46 -0700

    Update bbConfig.py

--- read_smartsheets/bbConfig.py
+++ read_smartsheets/bbConfig.py
@@ -1,11 +1,11 @@
 #Amazon Redshift connect string
-RED_HOST = "bi-prod-redshift-cluster.ctzjqu1zsbry.us-west-2.redshift.amazonaws.com"
-RED_DB =    "mount_olympus"
-RED_USER =  "bi_etl"
-RED_PASS =  "Mtwthfss!123"
+RED_HOST = "[DATABASE-HOSTNAME]"
+RED_DB =    "[DATABASE-HOSTNAME]"
+RED_USER =  "[DATABASE-HOSTNAME]"
+RED_PASS =  "[DATABASE-HOSTNAME]"
 RED_PORT =  "5439"
-AWS_ACCESS_KEY_ID =   "AKIAJV7MGKUC4CRGNYAA"
-AWS_SECRET_ACCESS_KEY =   "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"
+AWS_ACCESS_KEY_ID =   "[DATABASE-HOSTNAME]"
+AWS_SECRET_ACCESS_KEY =   "[DATABASE-HOSTNAME]"
 
 S3_BUCKET = "bi-etl-prod"
 S3_PREFIX = "temp"
